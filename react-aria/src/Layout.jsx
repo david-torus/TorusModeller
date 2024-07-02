fba054e27f81b98e47a43e9ca0dec302dd19ab7b
Allan Thomas <allanr@Torus.Tech>
2024-07-03 00:42:19 +0530

    changes

--- react-aria/src/Layout.jsx
+++ react-aria/src/Layout.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Navbar from "./Navbar";
+import ReactFlow, { MiniMap, Controls, Background } from "reactflow";
 
 export default function Layout() {
   return (
     <div className="bg-white xl:w-full 2xl:w-full lg:w-full md:w-full sm:w-full h-full">
       <Navbar />
-      <div></div>
+      <div style={{ width: "100vw", height: "95vh" }}>
+        <ReactFlow>
+          <Controls />
+          <MiniMap />
+          <Background variant="dots" gap={12} size={1} />
+        </ReactFlow>
+      </div>
     </div>
   );
 }
