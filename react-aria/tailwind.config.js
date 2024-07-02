97033c1d26a831ae32711a06458f9c3612298722
SelvaGaneshV <ganeshv@torus.tech>
2024-07-02 11:49:37 +0530

    animation keyframes

--- react-aria/tailwind.config.js
+++ react-aria/tailwind.config.js
@@ -2,7 +2,56 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        torusPopOverOpen: {
+          "0%": {
+            opacity: "0",
+          },
+          "25%": {
+            opacity: "0.25",
+          },
+          "50%": {
+            opacity: "0.5",
+          },
+          "75%": {
+            opacity: "0.75",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+        torusPopOverClose: {
+          "0%": {
+            opacity: "1",
+          },
+          "25%": {
+            opacity: "0.75",
+          },
+          "50%": {
+            opacity: "0.5",
+          },
+          "75%": {
+            opacity: "0.25",
+          },
+          "100%": {
+            opacity: "0",
+          },
+        },
+        torusButtonActive: {
+          "from, to": { transform: "scale(1, 1)" },
+          "25% ": { transform: "scale(0.9, 1.1)" },
+          "50%": { transform: "scale(1.1, 0.9)" },
+          "75%": { transform: "scale(0.95, 1.05)" },
+        },
+
+      },
+      animation: {
+        torusPopOverOpen: "torusPopOverOpen 150ms ease-in ",
+        torusPopOverClose: "torusPopOverClose 150ms ease-in",
+        torusButtonActive: "torusButtonActive 0.5s",
+      },
+    },
   },
   plugins: [require("tailwindcss-react-aria-components")({ prefix: "torus" })],
 };
