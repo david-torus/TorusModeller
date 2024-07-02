15974b3eb9ef4031e39ea8a066cf0809bd2f5392
SelvaGaneshV <ganeshv@torus.tech>
2024-07-02 12:42:40 +0530

    implemted dropdown halfway

--- react-aria/src/index.js
+++ react-aria/src/index.js
@@ -5,12 +5,15 @@ import "./index.css";
 import Testing from "./Testing";
 import Layout from "./Layout";
 import TorusModifiedInput from "./Input";
+import TorusDropDown from "./torusComponents/TorusDropDown";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <Testing /> */}
     <Layout />
+    {/* <App /> */}
     {/* <TorusModifiedInput value="test" placeholder="test" label="test" /> */}
   </React.StrictMode>
 );
