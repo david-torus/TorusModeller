870fe307a8dcfc5561a70e97f017319e6dc990d2
ManojGSS14 <manojk@torus.tech>
2024-07-09 13:03:33 +0530

    New component Created

--- react-aria/src/torusComponents/TorusSelector.jsx
+++ react-aria/src/torusComponents/TorusSelector.jsx
@@ -12,14 +12,11 @@ import { FaAngleDown } from "react-icons/fa6";
 import { IoIosCheckmark } from "react-icons/io";
 export default function TorusSelector({
   label,
-  items = [
-    { key: "Item 1", label: "Item 1" },
-    { key: "Item 2", label: "Item 2" },
-    { key: "Item 3", label: "Item 3" },
-  ],
+  items,
+  marginT
 }) {
   return (
-    <div className="flex justify-start w-[100%] pt-2">
+    <div className={`${marginT} flex justify-start w-[100%] pt-2`}>
       <Select
         className={
           "w-[100%] flex flex-col gap-0.5 bg-white rounded-md px-2 py-3 "
@@ -34,7 +31,11 @@ export default function TorusSelector({
             {(item) => (
               <ListBoxItem className="w-[100%] bg-slate-50 border-2 border-gray-100 transition-all p-1 rounded-md gap-1 flex flex-col items-center torus-hover:outline-none torus-hover:ring-0 torus-hover:border-transparent">
                 {({ isSelected }) => (
-                  <div className="w-full flex justify-between items-center torus-focus:outline-none ">
+                  <div
+                    className={`w-full ${
+                      isSelected ? "bg-slate-200 px-1 rounded-md py-2" : ""
+                    } flex justify-between items-center torus-focus:outline-none torus-hover:outline-none torus-hover:ring-0 torus-hover:border-none `}
+                  >
                     <Heading className="  3xl:text-xs xl:text-sm font-normal  tracking-tighter">
                       {item.label}
                     </Heading>
@@ -53,19 +54,20 @@ export default function TorusSelector({
               </ListBoxItem>
             )}
           </ListBox>
-          
         </Popover>
         <Button
           className={
             "flex justify-between items-center torus-pressed:border-transparent torus-focus:outline-none torus-pressed:ring-0  "
           }
-          
         >
           <div className="w-[50%] flex justify-start">
             <SelectValue className="text-sm text-[#000000]" />
           </div>
           <div className="w-[50%] flex justify-end" aria-hidden="true">
+            <span className="pr-1">
+
             <FaAngleDown size={15} />
+            </span>
           </div>
         </Button>
       </Select>
