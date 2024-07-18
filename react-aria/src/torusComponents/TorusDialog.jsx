5c6caeeef8f67bd1a53ca370f2c9aa344c1091a6
allanTorus <allanr@Torus.Tech>
2024-07-18 13:10:23 +0530

    gg

--- react-aria/src/torusComponents/TorusDialog.jsx
+++ react-aria/src/torusComponents/TorusDialog.jsx
@@ -8,19 +8,25 @@ import {
 } from "react-aria-components";
 import { merger } from "../utils/utils";
 
-export default function TorusDialog({ triggerElement, children, classNames }) {
+export default function TorusDialog({
+  triggerElement,
+  children,
+  classNames,
+  isOpen,
+}) {
   return (
     <DialogTrigger>
       {triggerElement}
       <ModalOverlay
         isDismissable
+        isOpen={isOpen}
         className={merger(
-          "fixed z-[100] top-0 left-0 w-screen h-screen bg-transparent/45 flex items-center justify-center",
+          "fixed z-[100] top-0 left-0 w-screen h-screen bg-transparent/45 flex  justify-center",
           classNames?.modalClassName
         )}
       >
-        <Modal  isDismissable>
-          <Dialog className={merger("", classNames?.dialogClassName)}>
+        <Modal isDismissable>
+          <Dialog className="border-none torus-focus:border-none torus-focus-within:border-none torus-focus-visible:border-none">
             {children}
           </Dialog>
         </Modal>
@@ -28,6 +34,3 @@ export default function TorusDialog({ triggerElement, children, classNames }) {
     </DialogTrigger>
   );
 }
-
-
-
