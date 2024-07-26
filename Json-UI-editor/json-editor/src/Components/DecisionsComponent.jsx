34c8cd13bee97a030a9fe5ffe69949972a847795
ManojGSS14 <manojk@torus.tech>
2024-07-26 10:40:07 +0530

    newchanges On tabs

---
+++ Json-UI-editor/json-editor/src/Components/DecisionsComponent.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DecisionsComponent() {
+  return <div>DecisionsComponent</div>;
+}
--- Json-UI-editor/json-editor/src/Editor.jsx
+++ Json-UI-editor/json-editor/src/Editor.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Button, Input, Tab, Tabs } from "@nextui-org/react";
 import { Select, SelectItem } from "@nextui-org/react";
+import FactsComponent from "./Components/FactsComponent";
+import DecisionsComponent from "./Components/DecisionsComponent";
+import ValidationsComponent from "./Components/ValidationsComponent";
+import GenerateComponent from "./Components/GenerateComponent";
 
 export default function Editor() {
-  const mainHeadings = ["Facts", "Decisions", "Validations", "Events"];
+  const mainHeadings = ["Facts", "Decisions", "Validations", "Generate"];
 
   return (
     <div className="w-full h-[100%] p-2">
@@ -18,6 +22,7 @@ export default function Editor() {
             cursor: "w-full bg-[#22d3ee]",
             tab: "max-w-fit px-0 h-12",
             tabContent: "group-data-[selected=true]:text-[#06b6d4]",
+            panel: "w-full h-[83vh] p-2",
           }}
         >
           {mainHeadings.map((heading, index) => (
@@ -32,6 +37,24 @@ export default function Editor() {
                   <FactsComponent />
                 </div>
               )}
+
+              {heading === "Decisions" && (
+                <div className="w-full flex justify-center p-2 ">
+                  <DecisionsComponent />
+                </div>
+              )}
+
+              {heading === "Validations" && (
+                <div className="w-full flex justify-center p-2 ">
+                  <ValidationsComponent />
+                </div>
+              )}
+
+              {heading === "Generate" && (
+                <div className="w-full flex justify-center p-2 ">
+                  <GenerateComponent />
+                </div>
+              )}
             </Tab>
           ))}
         </Tabs>
@@ -39,143 +62,3 @@ export default function Editor() {
     </div>
   );
 }
-
-export const FactsComponent = () => {
-  const [facts, setFacts] = useState([{ id: 1 }]); // Initialize with one component
-  const [selectedType, setSelectedType] = useState("");
-
-  useEffect(() => {
-    console.log(facts, "FACTSt"); // Initialize facts state if needed
-  }, [facts]);
-
-  const handleTypeSelection = (e) => {
-    console.log(e, "Events");
-    setSelectedType(e);
-  };
-
-  const dataTypes = ["string", "number", "boolean", "array", "object"];
-
-  const addComponent = () => {
-    if (facts.length === 0) {
-      setFacts([...facts, { id: facts.length + 1 }]); // Add a new component
-    }
-  };
-
-  const removeComponent = (id) => {
-    setFacts(facts.filter((fact) => fact.id !== id)); // Remove a specific component
-  };
-
-  const resetActions = () => {
-    setFacts([]);
-  };
-
-  return (
-    <div className="w-full h-[100%] p-2">
-      <div className="bg-slate-400/25 h-90 rounded-md shadow-sm p-2">
-        <div className="w-full flex justify-end">
-          <div className="w-[50%] flex flex-col justify-end">
-            <div className="w-[100%] flex justify-around gap-[0.5rem] bg-slate-400/15 p-2">
-              <Button
-                size="md"
-                color="transparent"
-                radius="md"
-                endContent={<p>+</p>}
-                onClick={addComponent}
-              >
-                ADD
-              </Button>
-              <Button onClick={resetActions}>RESET</Button>
-              <div className="w-[30%]">
-                <Input
-                  clearable
-                  label="Search"
-                  labelPlacement="inside"
-                  variant="bordered"
-                  className="w-[100%]"
-                  classNames={{
-                    inputWrapper: ["h-[30px] w-[100%]"],
-                  }}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        {facts.length === 0 ? (
-          <div className="w-full h-[100%] p-2 flex justify-center items-center">
-            <div className="w-full bg-red-500/40 border-dashed border-spacing-1 border-2 border-red-600  p-2">
-              <div>
-                <h2 className="text-lg font-semibold text-black">No Facts</h2>
-                <div className="flex justify-between ">
-                  <div className="w-[50%] flex justify-start text-black font-normal">
-                    There is no facts available in the selected ruleset
-                  </div>
-                  <div className="w-[50%] flex justify-end">
-                    <Button onClick={addComponent}>Create Facts</Button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        ) : (
-          <div className="w-full h-[100%] p-2">
-            <div className="w-full h-[390px]  overflow-y-auto scrollbar-hide">
-              <div
-                className="mt-2 w-[100%] flex flex-col gap-[0.5rem] bg-slate-400/10 p-2"
-                id="repeatedComponent"
-              >
-                {facts.map((fact) => (
-                  <div
-                    key={fact.id}
-                    className="w-[100%] grid grid-cols-12 gap-4 p-2 border-b border-divider"
-                  >
-                    <div className="col-span-6">
-                      <Input
-                        placeholder="Name"
-                        clearable
-                        label="Name"
-                        labelPlacement="inside"
-                        variant="bordered"
-                        className="w-[80%]"
-                      />
-                      <div className="w-[100%] flex justify-around gap-[0.5rem] pt-10">
-                        <Button size="sm" color="success" radius="md">
-                          Add Facts{" "}
-                        </Button>
-                        <Button
-                          size="sm"
-                          color="default"
-                          radius="md"
-                          onClick={() => removeComponent(fact.id)}
-                        >
-                          Cancel{" "}
-                        </Button>
-                      </div>
-                    </div>
-                    <div className="col-span-6">
-                      <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-                        <Select
-                          label="Favorite type"
-                          placeholder="Select a type"
-                          className="w-[80%]"
-                          variant="bordered"
-                          selectedKeys={selectedType}
-                          onSelectionChange={handleTypeSelection}
-                        >
-                          {dataTypes.map((type) => (
-                            <SelectItem key={type} value={type}>
-                              {type}
-                            </SelectItem>
-                          ))}
-                        </Select>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
-    </div>
-  );
-};
