49458b82db06beaaa3ff4fc7e4e15434c36ccb30
ManojGSS14 <manojk@torus.tech>
2024-07-29 10:42:11 +0530

    editor changes

--- Json-UI-editor/json-editor/src/Components/FactsComponent.jsx
+++ Json-UI-editor/json-editor/src/Components/FactsComponent.jsx
@@ -1,34 +1,45 @@
 import { Button, Input, Select, SelectItem } from "@nextui-org/react";
 import { useEffect, useState } from "react";
+import { FiEdit } from "@react-icons/all-files/fi/FiEdit";
+import { FiDelete } from "@react-icons/all-files/fi/FiDelete";
 
-export default function FactsComponent() {
-  const [facts, setFacts] = useState([{ id: 1, name: "", type: "" }]);
-  const [selectedType, setSelectedType] = useState("number");
+export default function FactsComponent({ factsVariables, setFactsVariables }) {
+  const [selectedType, setSelectedType] = useState("");
   const [editingFactId, setEditingFactId] = useState(null);
 
   useEffect(() => {
-    console.log(facts, "FACTS");
+    console.log(factsVariables, "FACTS");
     console.log(selectedType, "selected type");
-  }, [facts, selectedType]);
+    setFactsVariables(factsVariables);
+  }, [factsVariables, selectedType]);
 
   const dataTypes = [
     { key: "string", label: "string" },
     { key: "number", label: "number" },
-    { key: "boolean", label: "boolean" },
     { key: "array", label: "array" },
     { key: "object", label: "object" },
   ];
 
   const addComponent = () => {
-    setFacts([...facts, { id: facts.length + 1, name: "", type: "" }]);
+    if (
+      factsVariables[factsVariables.length - 1].name !== "" ||
+      factsVariables[factsVariables.length - 1].type !== ""
+    ) {
+      setFactsVariables([
+        ...factsVariables,
+        { id: factsVariables.length + 1, name: "", type: "" },
+      ]);
+
+      console.log("Trueeee=====>>>");
+    }
   };
 
   const removeComponent = (id) => {
-    setFacts(facts.filter((fact) => fact.id !== id));
+    setFactsVariables(factsVariables.filter((fact) => fact.id !== id));
   };
 
   const resetActions = () => {
-    setFacts([]);
+    setFactsVariables([]);
     setEditingFactId(null);
   };
 
@@ -38,7 +49,7 @@ export default function FactsComponent() {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFacts((prevFacts) =>
+    setFactsVariables((prevFacts) =>
       prevFacts.map((fact) =>
         fact.id === parseInt(name) ? { ...fact, name: value } : fact
       )
@@ -46,7 +57,7 @@ export default function FactsComponent() {
   };
 
   const saveChanges = (id) => {
-    const updatedFacts = facts.map((fact) => {
+    const updatedFacts = factsVariables.map((fact) => {
       if (fact.id === id) {
         return {
           ...fact,
@@ -55,7 +66,7 @@ export default function FactsComponent() {
       }
       return fact;
     });
-    setFacts(updatedFacts);
+    setFactsVariables(updatedFacts);
     setEditingFactId(null); // Exit edit mode
   };
 
@@ -65,95 +76,148 @@ export default function FactsComponent() {
 
   return (
     <div className="w-full h-[100%] p-2">
-      <div className="bg-slate-400/25 max-h-[100%] rounded-md shadow-sm p-2">
-        <div className="w-full flex justify-end">
-          <div className="w-[50%] flex flex-col justify-end">
-            <div className="w-[100%] flex justify-around gap-[0.5rem] bg-slate-400/15 p-2">
-              <Button
-                size="md"
-                color="transparent"
-                radius="md"
-                endContent={<p>+</p>}
-                onClick={addComponent}
-              >
-                ADD
-              </Button>
-              <Button onClick={resetActions}>RESET</Button>
-            </div>
+      <div className="bg-transparent  scrollbar-hide rounded-md shadow-sm p-2">
+        <div className="w-full flex justify-end px-2">
+          <div className="w-[50%] flex flex-row justify-end">
+            <Button
+              size="md"
+              endContent={<p>+</p>}
+              onClick={addComponent}
+              className="w-[30%] rounded-none bg-blue-400 text-white text-sm font-bold hover:bg-blue-500 hover:text-blue-800"
+            >
+              ADD
+            </Button>
+            <Button
+              className="w-[30%] rounded-none bg-teal-400 text-white text-sm font-bold hover:bg-teal-500 hover:text-teal-800"
+              onClick={resetActions}
+            >
+              RESET
+            </Button>
           </div>
         </div>
-        {facts.length === 0 ? (
-          <div className="w-full h-[100%] p-2 flex justify-center items-center">
-            <div className="w-full bg-red-500/40 border-dashed border-spacing-1 border-2 border-red-600 p-2">
-              <div>
-                <h2 className="text-lg font-semibold text-black">No Facts</h2>
-                <div className="flex justify-between">
-                  <div className="w-[50%] flex justify-start text-black font-normal">
-                    There are no facts available in the selected ruleset
-                  </div>
-                  <div className="w-[50%] flex justify-end">
-                    <Button onClick={addComponent}>Create Facts</Button>
+        <div className="max-h-[425px] overflow-y-scroll scrollbar-hide">
+          {factsVariables.length === 0 ? (
+            <div className="w-full p-2 flex justify-center items-center">
+              <div className="w-full bg-red-500/40 border-dashed border-spacing-1 border-2 border-red-600 p-2">
+                <div>
+                  <h2 className="text-lg font-semibold text-black">No Facts</h2>
+                  <div className="flex justify-between">
+                    <div className="w-[50%] flex justify-start text-black font-normal">
+                      There are no factsVariables available in the selected
+                      ruleset
+                    </div>
+                    <div className="w-[50%] flex justify-end">
+                      <Button
+                        onClick={addComponent}
+                        className="w-[30%]  text-white text-sm font-bold hover:bg-red-500/60 hover:text-red-800"
+                        variant="bordered"
+                      >
+                        Create Facts
+                      </Button>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        ) : (
-          <div className="w-full h-[100%] p-2">
-            <div className="w-full overflow-y-auto scrollbar-hide">
-              {facts.map((fact) => (
-                <div key={fact.id}>
-                  {editingFactId === fact.id ? (
-                    <EditComponent
-                      fact={fact}
-                      handleChange={handleChange}
-                      saveChanges={saveChanges}
-                      removeComponent={removeComponent}
-                      dataTypes={dataTypes}
-                      setSelectedType={setSelectedType}
-                      selectedType={selectedType}
-                    />
-                  ) : (
-                    <div className="mt-3 w-[100%] flex justify-center items-center h-20 border-1 border-slate-800 rounded-sm shadow-md border-l-blue-500 border-l-4">
-                      <div className="grid grid-cols-12 w-[80%]">
-                        <div className="col-span-3 flex justify-center">
-                          <div className="w-[50%] flex justify-center items-center">
-                            {fact.id}
-                          </div>
-                        </div>
+          ) : (
+            <div className="w-full h-[100%] p-2">
+              <div className="w-full overflow-y-auto scrollbar-hide">
+                {factsVariables.map((fact) => (
+                  <div key={fact.id}>
+                    {editingFactId === fact.id ? (
+                      <EditComponent
+                        fact={fact}
+                        handleChange={handleChange}
+                        saveChanges={saveChanges}
+                        removeComponent={removeComponent}
+                        dataTypes={dataTypes}
+                        setSelectedType={setSelectedType}
+                        selectedType={selectedType}
+                      />
+                    ) : (
+                      <div
+                        className={`mt-3 w-[100%] flex justify-center items-center h-20 border-1 border-slate-800 rounded-sm shadow-md *:
+                    ${fact.id % 2 === 0 ? "bg-slate-200" : "bg-slate-300"}
 
-                        <div className="col-span-3 flex justify-center">
-                          <div className="w-[50%] flex justify-center items-center">
-                            {fact.name || "Name"}
+                    ${
+                      fact.type === "string"
+                        ? "border-l-blue-500 border-l-4"
+                        : fact.type === "number"
+                        ? "border-l-green-500 border-l-4"
+                        : fact.type === "array"
+                        ? "border-l-yellow-500 border-l-4"
+                        : fact.type === "object"
+                        ? "border-l-pink-500 border-l-4"
+                        : "border-l-slate-500 border-l-4"
+                    }
+                    
+                    `}
+                      >
+                        <div className="grid grid-cols-12 w-[100%]">
+                          <div className="col-span-3 flex justify-center">
+                            <div className="w-[50%] flex justify-center items-center font-semibold ">
+                              {fact.id}
+                            </div>
                           </div>
-                        </div>
 
-                        <div className="col-span-3 flex justify-center">
-                          <div className="w-[50%] flex justify-center items-center">
-                            {fact.type || "Type"}
+                          <div className="col-span-3 flex justify-center">
+                            <div
+                              className={`w-[100%] flex justify-center items-center font-bold text-sm
+                            text-slate-800
+                            `}
+                            >
+                              {fact.name || "Name"}
+                            </div>
                           </div>
-                        </div>
 
-                        <div className="col-span-3 flex justify-center">
-                          <div className="w-[90%] flex justify-end gap-2">
-                            <div className="w-[80%] flex justify-between gap-1">
-                              <Button onClick={() => startEditing(fact.id)}>
-                                Edit
-                              </Button>
-                              <Button onClick={() => removeComponent(fact.id)}>
-                                Delete
-                              </Button>
+                          <div className="col-span-3 flex justify-center">
+                            <div
+                              className={`w-[100%] flex justify-center items-center font-bold ${
+                                fact.type === "string"
+                                  ? "text-blue-800 bg-blue-400/75  p-1"
+                                  : fact.type === "number"
+                                  ? "text-green-800 bg-green-400/75  p-1"
+                                  : fact.type === "array"
+                                  ? "text-yellow-800 bg-yellow-400/75  p-1"
+                                  : fact.type === "object"
+                                  ? "text-pink-800 bg-pink-400/75  p-1"
+                                  : "text-slate-800 bg-slate-400/75  p-1"
+                              }`}
+                            >
+                              {fact.type || "Type"}
+                            </div>
+                          </div>
+
+                          <div className="col-span-3 flex justify-center">
+                            <div className="w-[90%] flex justify-center gap-2">
+                              <div className="w-[80%] flex ">
+                                <Button
+                                  onClick={() => startEditing(fact.id)}
+                                  className="w-[50%] rounded-none border-purple-400 text-white text-sm font-bold hover:border-purple-500 hover:text-purple-800"
+                                  isIconOnly
+                                  variant="bordered"
+                                >
+                                  <FiEdit size={20} color="purple" />
+                                </Button>
+                                <Button
+                                  className="w-[50%] rounded-none border-red-400 text-white text-sm font-bold hover:border-red-500 hover:text-red-800"
+                                  onClick={() => removeComponent(fact.id)}
+                                  variant="bordered"
+                                >
+                                  <FiDelete size={20} color="red" />
+                                </Button>
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  )}
-                </div>
-              ))}
+                    )}
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </div>
   );
@@ -169,7 +233,7 @@ const EditComponent = ({
   selectedType,
 }) => {
   return (
-    <div className="w-[100%] grid grid-cols-12 gap-4 p-2 border-b border-divider">
+    <div className="w-[100%] grid grid-cols-12 gap-4 py-3 px-2 border border-slate-800">
       <div className="col-span-6">
         <Input
           placeholder="Enter name"
@@ -181,13 +245,18 @@ const EditComponent = ({
           name={fact.id.toString()}
           value={fact.name}
           onChange={handleChange}
+          classNames={{
+            inputWrapper:
+              "rounded-none data-[open=true]:border-slate-800 data-[hover=true]:border-slate-800 data-[focus=true]:border-slate-800 border-slate-800 text-purple-800 font-bold",
+          }}
         />
-        <div className="w-[100%] flex justify-around gap-[0.5rem] pt-10">
+        <div className="w-[100%] flex pt-10">
           <Button
             size="sm"
             color="success"
             radius="md"
             onClick={() => saveChanges(fact.id)}
+            className="w-[30%] rounded-none bg-blue-400 text-white text-sm font-bold hover:bg-blue-500 hover:text-blue-800"
           >
             Save Changes
           </Button>
@@ -196,13 +265,14 @@ const EditComponent = ({
             color="default"
             radius="md"
             onClick={() => removeComponent(fact.id)}
+            className="w-[30%] rounded-none bg-red-400 text-white text-sm font-bold hover:bg-red-500 hover:text-red-800"
           >
             Clear
           </Button>
         </div>
       </div>
       <div className="col-span-6">
-        <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+        <div className="flex w-full flex-wrap justify-end md:flex-nowrap gap-4">
           <Select
             label="Favorite type"
             placeholder="Select a type"
@@ -211,6 +281,10 @@ const EditComponent = ({
             selectedKeys={selectedType ? [selectedType] : []}
             onSelectionChange={(e) => setSelectedType(Array.from(e)[0])}
             items={dataTypes}
+            classNames={{
+              trigger:
+                "rounded-none data-[open=true]:border-slate-800 data-[hover=true]:border-slate-800 data-[focus=true]:border-slate-800 border-slate-800 text-purple-800 font-bold",
+            }}
           >
             {(type) => (
               <SelectItem key={type.key} value={type.key}>
--- Json-UI-editor/json-editor/src/Editor.jsx
+++ Json-UI-editor/json-editor/src/Editor.jsx
@@ -1,61 +1,144 @@
-import React, { useContext, useEffect, useState } from "react";
-import { Button, Input, Tab, Tabs } from "@nextui-org/react";
-import { Select, SelectItem } from "@nextui-org/react";
+// import React, { useContext, useEffect, useState } from "react";
+// import { Button, Input, Tab, Tabs } from "@nextui-org/react";
+// import { Select, SelectItem } from "@nextui-org/react";
+// import FactsComponent from "./Components/FactsComponent";
+// import DecisionsComponent from "./Components/DecisionsComponent";
+// import ValidationsComponent from "./Components/ValidationsComponent";
+// import GenerateComponent from "./Components/GenerateComponent";
+// import DecisionView from "./Components/decisionView";
+// import { JsonUiEditorContext } from "./Layout";
+
+// export default function Editor() {
+//   const mainHeadings = ["Facts", "Decisions", "Validations", "Generate"];
+//   const [showDecision, setDecision] = useState(false);
+
+//   const [selectedDecison, setSelectedDecison] = useState(null);
+
+//   return (
+//     <div className="w-full h-[100%] p-2">
+//       <div className="bg-slate-400/25 rounded-md shadow-sm p-2">
+//         <Tabs
+//           aria-label="Options"
+//           color="primary"
+//           variant="underlined"
+//           classNames={{
+//             tabList:
+//               "gap-6 w-full relative rounded-none p-0 border-b border-divider",
+//             cursor: "w-full bg-[#22d3ee]",
+//             tab: "max-w-fit px-0 h-12",
+//             tabContent: "group-data-[selected=true]:text-[#06b6d4]",
+//             panel: "w-full h-[83vh] p-2",
+//           }}
+//         >
+//           {mainHeadings.map((heading, index) => (
+//             <Tab
+//               key={index}
+//               className="text-slate-400"
+//               title={heading}
+//               content={heading}
+//             >
+//               {heading === "Facts" && (
+//                 <div className="w-full flex justify-center p-2 ">
+//                   <FactsComponent />
+//                 </div>
+//               )}
+
+//               {heading === "Decisions" && (
+//                 <div className="w-full h-full flex justify-center p-2 ">
+//                   {showDecision ? (
+//                     <DecisionsComponent
+//                       selectedDecison={selectedDecison}
+//                       setDecision={setDecision}
+//                     />
+//                   ) : (
+//                     <DecisionView
+//                       setSelectedDecison={setSelectedDecison}
+//                       setDecision={setDecision}
+//                     />
+//                   )}
+//                 </div>
+//               )}
+
+//               {heading === "Validations" && (
+//                 <div className="w-full flex justify-center p-2 ">
+//                   <ValidationsComponent />
+//                 </div>
+//               )}
+
+//               {heading === "Generate" && (
+//                 <div className="w-full flex justify-center p-2 ">
+//                   <GenerateComponent />
+//                 </div>
+//               )}
+//             </Tab>
+//           ))}
+//         </Tabs>
+//       </div>
+//     </div>
+//   );
+// }
+
+
+import React, { useEffect, useState } from "react";
+import { Tab, Tabs } from "@nextui-org/react";
+import examplejson from "./example.json";
+
 import FactsComponent from "./Components/FactsComponent";
 import DecisionsComponent from "./Components/DecisionsComponent";
 import ValidationsComponent from "./Components/ValidationsComponent";
 import GenerateComponent from "./Components/GenerateComponent";
-import DecisionView from "./Components/decisionView";
-import { JsonUiEditorContext } from "./Layout";
 
 export default function Editor() {
   const mainHeadings = ["Facts", "Decisions", "Validations", "Generate"];
-  const [showDecision, setDecision] = useState(false);
-
-  const [selectedDecison, setSelectedDecison] = useState(null);
+  const [factsVariables, setFactsVariables] = useState([
+    { id: 1, name: "", type: "" },
+  ]);
+  const [json, setJson] = useState(examplejson);
 
   return (
     <div className="w-full h-[100%] p-2">
-      <div className="bg-slate-400/25 rounded-md shadow-sm p-2">
+      <div className="bg-slate-400/25  shadow-sm p-2">
         <Tabs
           aria-label="Options"
           color="primary"
           variant="underlined"
           classNames={{
             tabList:
-              "gap-6 w-full relative rounded-none p-0 border-b border-divider",
+              "gap-6 w-full relative rounded-none p-0 border-b border-divider text-sm font-medium text-slate-400",
             cursor: "w-full bg-[#22d3ee]",
             tab: "max-w-fit px-0 h-12",
             tabContent: "group-data-[selected=true]:text-[#06b6d4]",
             panel: "w-full h-[83vh] p-2",
           }}
+          disabledKeys={
+            (factsVariables[factsVariables.length - 1]?.name === "" &&
+            factsVariables[factsVariables.length - 1]?.type === "")
+              ? ["Decisions"]
+              : []
+          }
         >
           {mainHeadings.map((heading, index) => (
             <Tab
-              key={index}
+              key={heading}
               className="text-slate-400"
               title={heading}
               content={heading}
             >
               {heading === "Facts" && (
                 <div className="w-full flex justify-center p-2 ">
-                  <FactsComponent />
+                  <FactsComponent
+                    factsVariables={factsVariables}
+                    setFactsVariables={setFactsVariables}
+                  />
                 </div>
               )}
 
               {heading === "Decisions" && (
-                <div className="w-full h-full flex justify-center p-2 ">
-                  {showDecision ? (
-                    <DecisionsComponent
-                      selectedDecison={selectedDecison}
-                      setDecision={setDecision}
-                    />
-                  ) : (
-                    <DecisionView
-                      setSelectedDecison={setSelectedDecison}
-                      setDecision={setDecision}
-                    />
-                  )}
+                <div className="w-full flex justify-center p-2 ">
+                  <DecisionsComponent
+                    factsVariables={factsVariables}
+                    json={json}
+                  />
                 </div>
               )}
 
