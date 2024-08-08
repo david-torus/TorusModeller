b38bac7d2fd4dc0d6762f72bfd76c68bd1bde4f3
SelvaGaneshV <ganeshv@torus.tech>
2024-08-08 11:35:44 +0530

    implemnted artifacts details API given by TP Team

--- reactFlowMainBackend/src/VPT/commonVptServices.ts
+++ reactFlowMainBackend/src/VPT/commonVptServices.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 
 import { RedisService } from 'src/redisService';
 interface artifactInfo {
